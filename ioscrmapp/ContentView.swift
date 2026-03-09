4d1741faf012be18a1ecd1aef733089dc064cb04
MRAN <xrkj_yangjm@163.com>
2026-03-09 14:40:19 +0800

    Initial Commit

---
+++ ioscrmapp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ioscrmapp
+//
+//  Created by scaler on 2026/3/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ioscrmapp/ioscrmappApp.swift
@@ -0,0 +1,17 @@
+//
+//  ioscrmappApp.swift
+//  ioscrmapp
+//
+//  Created by scaler on 2026/3/9.
+//
+
+import SwiftUI
+
+@main
+struct ioscrmappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
